fc1cc27cac0c8c81a1cfd8f65502153d5372dfef
michaeldbjava <michael-laube@web.de>
2017-03-08 07:03:45 +0100

    Add class CheckCsvToDBConfigurationInformation. Next step is to implement this class

---
+++ src/main/java/de/rib/readcsv/CheckCsvToDBConfigurationInformation.java
@@ -0,0 +1,10 @@
+package de.rib.readcsv;
+
+public class CheckCsvToDBConfigurationInformation {
+private ConfigurationCsvToDB configCsvToDb;
+	public CheckCsvToDBConfigurationInformation(ConfigurationCsvToDB configCsvToDb) {
+		// TODO Auto-generated constructor stub
+		this.configCsvToDb=configCsvToDb;
+	}
+
+}
